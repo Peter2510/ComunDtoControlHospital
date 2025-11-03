8a62ff2bc37e1b414d90d2807603dff929fb3d0d
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2025-11-02 19:44:22 -0600

    feat: Se agrega constructor vacio a CuentaPagadaEvent

--- src/main/java/com/commonDto/dto/eventos/pacientes/cuenta/CuentaPagadaEvent.java
+++ src/main/java/com/commonDto/dto/eventos/pacientes/cuenta/CuentaPagadaEvent.java
@@ -2,22 +2,25 @@ package com.commonDto.dto.eventos.pacientes.cuenta;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class CuentaPagadaEvent {
 
-    private final UUID cuentaId;
-    private final UUID ingresoId;
-    private final String pacienteCui;
-    private final String medicoCui;
-    private final BigDecimal total;
-    private final LocalDateTime fechaPago;
+    private UUID cuentaId;
+    private UUID ingresoId;
+    private String pacienteCui;
+    private String medicoCui;
+    private BigDecimal total;
+    private LocalDateTime fechaPago;
 
 
 }
